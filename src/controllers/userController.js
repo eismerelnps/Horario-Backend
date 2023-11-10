97f2ca03c1e7be9469ab908c1e3ca01ff679b565
David Velázquez Morales <101413618+DaytonPlus@users.noreply.github.com>
2023-11-10 02:04:11 -0500

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -55,18 +55,21 @@ exports.createUser = async (req, res) => {
 // Función para verificar las credenciales del usuario y generar un token JWT
 exports.login = async (req, res) => {
     try {
-      const { username, password } = req.body;
+      const { identifier, password } = req.body;
   
-      // Buscar al usuario en la base de datos
-      const findUser = await User.findOne({ username });
+      // Buscar al usuario o email  en la base de datos
+      const findUser = await User.findOne({
+        $or: [{ username: identifier }, { email: identifier }]
+      });
+      
       if (!findUser) {
-        return res.status(401).json({ message: 'Usuario no encontrado' });
+        return res.status(401).json({ message: 'usuario o contraseña incorrectos' });
       }
   
       // Verificar la contraseña
       const passwordMatch = await bcrypt.compare(password, findUser.password);
       if (!passwordMatch) {
-        return res.status(401).json({ message: 'Contraseña incorrecta' });
+        return res.status(401).json({ message: 'usuario o contraseña incorrectos' });
       }
 
       // Crear contenedor con los datos que seran enviados
