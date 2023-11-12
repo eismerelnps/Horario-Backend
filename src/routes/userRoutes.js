a4db5f7c59ce556515803fc80d84572759db1228
David Velázquez Morales <101413618+DaytonPlus@users.noreply.github.com>
2023-11-12 09:12:23 -0500

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const userController = require('../controllers/userController');
+const middleware = require('../middleware/middleware');
 
 const router = express.Router();
 
@@ -9,17 +10,18 @@ const router = express.Router();
 // Ruta para autenticacion de un usuario
 router.post('/login', userController.login);
 
-// Ruta para comprobar el estado del token del usuario
-router.post('/check', userController.check);
-
 // Ruta para crear un nuevo usuario
 router.post('/create-user', userController.createUser);
 
+
+// Ruta para comprobar el estado del token del usuario
+router.post('/check', middleware.authenticate, userController.check);
+
 // Ruta para actualizar los datos de un usuario
-router.post('/update-user', userController.update);
+router.put('/update-user', middleware.authenticate, userController.update);
 
 // Ruta para eliminar un usuario
-router.post('/delete-user', userController.delete);
+router.delete('/delete-user', middleware.authenticate, userController.delete);
 
 
 module.exports = router;
