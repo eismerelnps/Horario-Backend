3fa27a8c33b57d670bd5867855af322a64aebd78
David Velázquez Morales <101413618+DaytonPlus@users.noreply.github.com>
2023-11-14 18:31:04 -0500

    Update hourController.js

--- src/controllers/hourController.js
+++ src/controllers/hourController.js
@@ -2,243 +2,80 @@
 
 const logger = require('../logs/logger');
 const { School } = require('../models/schoolModel');
-
 const ObjectId = require("mongoose").Types.ObjectId;
 
 
 /**  FUNCIONES BASICAS  **/
 
-exports.getFromFaculties = async (req, res) => {
-  try {
-    const { faculty } = req.params;
-    let find = null;
-    let id = null;
-
-    find = School.findOne({ id: req.user.school });
-    if (!find) return res.status(404).send({ message: `La Escuela de id = ${req.user.school}' no existe!`, data: null });
-
-    if (faculty === 'all') {
-      find = await find.faculties.find();
-      if (!find) return res.status(404).send({ message: 'No hay elementos!', data: null });
-    }
-    else  {
-      id = convertToNumber(faculty);
-      if(typeof id !== 'number') throw id;
-      find = await find.faculties.findOne({ id });
-      if (!find) return res.status(404).send({ message: `La facultad de id = '${id}' no existe!`, data: null });
-    }
-
-    return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find });
-  } catch (error) {
-    return res.status(500).send({ message: 'Error al obtener los datos', error });
-  }
-}
-
-exports.getFromYears = async (req, res) => {
-  try {
-    const { faculty, year } = req.params;
-    let find = null;
-    let id = null;
-
-    find = School.findOne({ id: req.user.school });
-    if (!find) return res.status(404).send({ message: `La Escuela de id = ${req.user.school}' no existe!`, data: null });
-
-
-    id = convertToNumber(faculty);
-    if(typeof id !== 'number') throw id;
-    find = await find.faculties.findOne({ id });
-    if (!find) return res.status(404).send({ message: `La Facultad de id = '${id}' no existe!`, data: null });
-
-    if(faculty === 'all') {
-      find = await find.years.find();
-      if (!find) return res.status(404).send({ message: 'No hay elementos!', data: null });
-    }
-    else  {
-      id = convertToNumber(year);
-      if(typeof id !== 'number') throw id;
-      find = await find.years.findOne({ id });
-      if (!find) return res.status(404).send({ message: `El año de id = '${id}' no existe!`, data: null });
-    }
-
-    return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find });
-  } catch (error) {
-    return res.status(500).send({ message: 'Error al obtener los datos', error });
-  }
-}
-
-exports.getFromGroups = async (req, res) => {
-  try {
-    const { faculty, year, group } = req.params;
-    let find = null;
-    let id = null;
-
-    find = School.findOne({ id: req.user.school });
-    if (!find) return res.status(404).send({ message: `La Escuela de id = ${req.user.school}' no existe!`, data: null });
-
-    id = convertToNumber(faculty);
-    if(typeof id !== 'number') throw id;
-    find = await find.faculties.findOne({ id });
-    if (!find) return res.status(404).send({ message: `La Facultad de id = '${id}' no existe!`, data: null });
-
-   id = convertToNumber(year);
-   if(typeof id !== 'number') throw id;
-   find = await find.years.findOne({ id });
-   if (!find) return res.status(404).send({ message: `El año de id = '${id}' no existe!`, data: null });
-
-    if(faculty === 'all') {
-      find = await find.groups.find();
-      if (!find) return res.status(404).send({ message: 'No hay elementos!', data: null });
-    }
-    else  {
-      id = convertToNumber(group);
-      if(typeof id !== 'number') throw id;
-      find = await find.groups.findOne({ id });
-      if (!find) return res.status(404).send({ message: `El grupo de id = '${id}' no existe!`, data: null });
-    }
-
-    return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find });
-  } catch (error) {
-    return res.status(500).send({ message: 'Error al obtener los datos', error });
-  }
-}
-
-exports.getFromWeeks = async (req, res) => {
-  try {
-    const { faculty, year, group, week } = req.params;
-    let find = null;
-    let id = null;
-
-    find = School.findOne({ id: req.user.school });
-    if (!find) return res.status(404).send({ message: `La Escuela de id = ${req.user.school}' no existe!`, data: null });
-
-    id = convertToNumber(faculty);
-    if(typeof id !== 'number') throw id;
-    find = await find.faculties.findOne({ id });
-    if (!find) return res.status(404).send({ message: `La Facultad de id = '${id}' no existe!`, data: null });
-
-   id = convertToNumber(year);
-   if(typeof id !== 'number') throw id;
-   find = await find.years.findOne({ id });
-   if (!find) return res.status(404).send({ message: `El año de id = '${id}' no existe!`, data: null });
-
-   id = convertToNumber(group);
-   if(typeof id !== 'number') throw id;
-   find = await find.years.findOne({ id });
-   if (!find) return res.status(404).send({ message: `El año de id = '${id}' no existe!`, data: null });
-
-    if(faculty === 'all') {
-      find = await find.weeks.find();
-      if (!find) return res.status(404).send({ message: 'No hay elementos!', data: null });
-    }
-    else  {
-      id = convertToNumber(week);
-      if(typeof id !== 'number') throw id;
-      find = await find.weeks.findOne({ id });
-      if (!find) return res.status(404).send({ message: `La semana de id = '${id}' no existe!`, data: null });
-    }
-
-    return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find });
-  } catch (error) {
-    return res.status(500).send({ message: 'Error al obtener los datos', error });
-  }
-}
-
-exports.getFromSchedules = async (req, res) => {
-  try {
-    const { faculty, year, group, week, schedule } = req.params;
-    let find = null;
-    let id = null;
-
-    find = School.findOne({ id: req.user.school });
-    if (!find) return res.status(404).send({ message: `La Escuela de id = ${req.user.school}' no existe!`, data: null });
-
-    id = convertToNumber(faculty);
-    if(typeof id !== 'number') throw id;
-    find = await find.faculties.findOne({ id });
-    if (!find) return res.status(404).send({ message: `La Facultad de id = '${id}' no existe!`, data: null });
-
-   id = convertToNumber(year);
-   if(typeof id !== 'number') throw id;
-   find = await find.years.findOne({ id });
-   if (!find) return res.status(404).send({ message: `El año de id = '${id}' no existe!`, data: null });
-
-   id = convertToNumber(group);
-   if(typeof id !== 'number') throw id;
-   find = await find.groups.findOne({ id });
-   if (!find) return res.status(404).send({ message: `El año de id = '${id}' no existe!`, data: null });
-
-   id = convertToNumber(week);
-   if(typeof id !== 'number') throw id;
-   find = await find.weeks.findOne({ id });
-   if (!find) return res.status(404).send({ message: `La semana de id = '${id}' no existe!`, data: null });
-
-    if(faculty === 'all') {
-      find = await find.schedules.find();
-      if (!find) return res.status(404).send({ message: 'No hay elementos!', data: null });
-    }
-    else  {
-      id = convertToNumber(schedule);
-      if(typeof id !== 'number') throw id;
-      find = await find.schedules.findOne({ id });
-      if (!find) return res.status(404).send({ message: `El horario de id = '${id}' no existe!`, data: null });
-    }
-
-    return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find });
-  } catch (error) {
-    return res.status(500).send({ message: 'Error al obtener los datos', error });
-  }
-}
-
-exports.getFromClasses = async (req, res) => {
+exports.getFormAll = async (req, res) => {
   try {
     const { faculty, year, group, week, schedule, sclass } = req.params;
-    let find = null;
-    let id = null;
+    
+    // Escuela
+    const find = School.findOne({ id: req.user.school });
+    if (!find) throw { code: 2, name: ' la Escuela', id: req.user.school };
 
-    find = School.findOne({ id: req.user.school });
-    if (!find) return res.status(404).send({ message: `La Escuela de id = ${req.user.school}' no existe!`, data: null });
-
-    id = convertToNumber(faculty);
+    // Facultad
+    if (!find.faculties || find.faculties.length === 0) throw { code: 3 };
+    if (faculty == 'all') return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties });
+    let id = convertToNumber(faculty);
     if(typeof id !== 'number') throw id;
-    find = await find.faculties.findOne({ id });
-    if (!find) return res.status(404).send({ message: `La Facultad de id = '${id}' no existe!`, data: null });
+    let elm = find.faculties.findIndex(e => e.id === id);
+    if (elm < 0) throw { code: 2, name: ' la Facultad', id };
+    if (!year) return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm] });
 
-   id = convertToNumber(year);
-   if(typeof id !== 'number') throw id;
-   find = await find.years.findOne({ id });
-   if (!find) return res.status(404).send({ message: `El año de id = '${id}' no existe!`, data: null });
+    // Año
+    if(!find.faculties[elm].years || find.faculties[elm].years.length === 0) throw { code: 3 };
+    if (years == 'all') return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years });
+    id = convertToNumber(year);
+    if(typeof id !== 'number') throw id;
+    let elm2 = find.faculties[elm].years.findIndex(e => e.id === id);
+    if (elm2 < 0) throw { code: 2, name: "l Año", id };
+    if (!group) return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years[elm2] });
 
-   id = convertToNumber(group);
-   if(typeof id !== 'number') throw id;
-   find = await find.groups.findOne({ id });
-   if (!find) return res.status(404).send({ message: `El año de id = '${id}' no existe!`, data: null });
+    // Grupo
+    if(!find.faculties[elm].years[elm2].groups || find.faculties[elm].years[elm2].groups.length === 0) throw { code: 2 };
+    if (group == 'all') return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years[elm2].groups });
+    id = convertToNumber(group);
+    if(typeof id !== 'number') throw id;
+    let elm3 = find.faculties[elm].years[elm2].groups.findIndex(e => e.id === id);
+    if (elm3 < 0) throw { code: 2, name: "l Grupo", id };
+    if (!week) return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years[elm2].groups[elm3] });
+    
+    // Semana
+    if (!find.faculties[elm].years[elm2].groups[elm3].weeks || find.faculties[elm].years[elm2].groups[elm3].weeks.length === 0) throw { code: 2 };
+    if (years == 'all') return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years[elm2].groups[elm3].weeks });
+    id = convertToNumber(week);
+    if(typeof id !== 'number') throw id;
+    let elm4 = find.faculties[elm].years[elm2].groups[elm3].weeks.findIndex(e => e.id === id);
+    if (elm4 < 0) throw { code: 2, name: " la Semana", id };
+    if (!schedule) return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years[elm2].groups[elm3].weeks[elm4] });
+    
+    // Horario
+    if (!find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules || find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules.length === 0) throw { code: 2 };
+    if (years == 'all') return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules });
+    id = convertToNumber(schedule);
+    if(typeof id !== 'number') throw id;
+    let elm5 = find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules.findIndex(e => e.id === id);
+    if (elm5 < 0) throw { code: 2, name: "l Horario", id };
+    if (!sclass) return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules[elm5] });
 
-   id = convertToNumber(week);
-   if(typeof id !== 'number') throw id;
-   find = await find.weeks.findOne({ id });
-   if (!find) return res.status(404).send({ message: `La semana de id = '${id}' no existe!`, data: null });
-
-   id = convertToNumber(schedule);
-   if(typeof id !== 'number') throw id;
-   find = await find.schedules.findOne({ id });
-   if (!find) return res.status(404).send({ message: `El horario de id = '${id}' no existe!`, data: null });
-
-    if(faculty === 'all') {
-      find = await find.classes.find();
-      if (!find) return res.status(404).send({ message: 'No hay elementos!', data: null });
-    }
-    else  {
-      id = convertToNumber(schedule);
-      if(typeof id !== 'number') throw id;
-      find = await find.classes.findOne({ id });
-      if (!find) return res.status(404).send({ message: `La clase de id = '${id}' no existe!`, data: null });
-    }
-
-    return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find });
+    // Clase
+    if (!find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules[elm5].classes || find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules[elm].classes.length === 0) throw { code: 2 };
+    if (years == 'all') return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules.classes });
+    id = convertToNumber(sclass);
+    if(typeof id !== 'number') throw id;
+    let elm6 = !find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules[elm5].classes.findIndex(e => e.id === id);
+    if (elm6 < 0) throw { code: 2, name: " la clase", id };
+    return res.status(200).send({ message: 'Datos obtenidos correctamente', data: find.faculties[elm].years[elm2].groups[elm3].weeks[elm4].schedules[elm5].classes[elm6] });
+    
   } catch (error) {
-    return res.status(500).send({ message: 'Error al obtener los datos', error });
+    if(err.code === 2) return res.status(500).send({ message: `El id '${error.id}' de${error.name} no existe!`, data: null });
+    if(error.code === 3) return res.status(500).send({ message: 'No hay elementos!', data: null });
+    return res.status(500).send({ message: 'Error al obtener los datos!', data: null });
   }
 }
+
 
 
 function convertToNumber(value) {
@@ -246,13 +83,13 @@ function convertToNumber(value) {
     if (isNaN(value))  {
        throw {
           name: 'isNaN',
-          message: 'No se puede convertir un texto no numérico!',
+          message: 'No se puede convertir un texto no num�rico!',
           data: { value },
       };
     }
     return Number(value);
   } catch (e) {
     return e;
-  }
 }
-       
+
+
